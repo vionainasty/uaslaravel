ab031e5ae20413ade580418e5b8ca639a6d9ffc7
vionainasty <vionainasty22@gmail.com>
2025-07-01 18:38:33 +0700

    view

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
