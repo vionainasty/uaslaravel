a82103d4d3ad5ac905d105969c30053a0bcd7792
vionainasty <vionainasty22@gmail.com>
2025-07-03 08:03:11 +0700

    Membuat Model

---
+++ app/Models/Post.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+class Post
+{
+private static $blog_posts = [
+    [
+        "title" => "Judul Post Pertama",
+        "slug" => "judul-post-pertama",
+        "author" => "Ravi",
+        "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, cumque. Lorem ipsum 
+        dolor sit amet consectetur adipisicing elit. Doloribus, cumque. Lorem ipsum dolor sit amet consectetur 
+        adipisicing elit. Doloribus, cumque."
+    ],
+    [
+        "title" => "Judul Post Kedua",
+        "slug" => "judul-post-kedua",
+        "author" => "Rangga",
+        "body" => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore dicta porro tenetur 
+        quas eos labore quis nostrum suscipit ratione omnis?"
+    ]
+];
+
+public static function all()
+{
+    return collect(self::$blog_posts);
+
+}
+public static function find($slug)
+{
+    $posts = self::$blog_posts;
+$post = [];
+foreach($posts as $p) {
+  if($p["slug"] === $slug) {
+    $post = $p;
+}
+}
+return $post;
+}
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('home', [
@@ -23,60 +25,21 @@ Route::get('/about', function () {
 Route::get('/blog', function () {
 
 $blog_posts = [
-    [
-        "title" => "Judul Post Pertama",
-        "slug" => "judul-post-pertama",
-        "author" => "Ravi",
-        "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, cumque. Lorem ipsum 
-        dolor sit amet consectetur adipisicing elit. Doloribus, cumque. Lorem ipsum dolor sit amet consectetur 
-        adipisicing elit. Doloribus, cumque."
-    ],
-    [
-        "title" => "Judul Post Kedua",
-        "slug" => "judul-post-kedua",
-        "author" => "Rangga",
-        "body" => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore dicta porro tenetur 
-        quas eos labore quis nostrum suscipit ratione omnis?"
-    ],
+
 ];
 
 
     return view('posts', [
         "title" => "Posts",
-        "posts" => $blog_posts
+        "posts" => Post::all()
     ]);
 });
 
 //halaman single post
 Route::get('posts/{slug}', function($slug) {
 
-$blog_posts = [
-    [
-        "title" => "Judul Post Pertama",
-        "slug" => "judul-post-pertama",
-        "author" => "Ravi",
-        "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, cumque. Lorem ipsum 
-        dolor sit amet consectetur adipisicing elit. Doloribus, cumque. Lorem ipsum dolor sit amet consectetur 
-        adipisicing elit. Doloribus, cumque."
-    ],
-    [
-        "title" => "Judul Post Kedua",
-        "slug" => "judul-post-kedua",
-        "author" => "Rangga",
-        "body" => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore dicta porro tenetur 
-        quas eos labore quis nostrum suscipit ratione omnis?"
-    ],
-];
-
-$new_post = [];
-foreach($blog_posts as $post) {
-    if($post["slug"] === $slug) {
-        $new_post = $post;
-    }
-}
-
 return view('post', [
 "title" => "Single Post",
-"post" => $new_post
+"post" => Post::find($slug)
 ]);
 });
